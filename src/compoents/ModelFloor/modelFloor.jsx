05107db3f59904ea137c1eb04c862e2baba85a6f
Aneesh <brainchildofbalan@gmail.com>
2024-01-14 13:35:36 +0530

    added new code

--- src/compoents/ModelFloor/modelFloor.jsx
+++ src/compoents/ModelFloor/modelFloor.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Html, OrbitControls, Stage, useProgress } from "@react-three/drei";
 import * as THREE from "three";
@@ -6,12 +6,34 @@ import Mesh from "../Mesh";
 
 const ModelFloor = ({ sole, stripe, back, top }) => {
   const lightRef = useRef();
-  console.log(lightRef);
+  const [progressNum, setprogressNum] = useState(0);
+
+  const { progress } = useProgress();
+
+
+  useEffect(() => {
+    let interval;
+
+    interval = setInterval(() => {
+      setprogressNum(progress)
+      if (progress === 100) {
+        clearInterval(interval);
+      }
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [progress]);
+
+
+
+
 
   function Loader() {
     const { active, progress, errors, item, loaded, total } = useProgress();
 
-    return <Html center>{Math.floor(progress)}%</Html>;
+    return <Html center>{Math.floor(progressNum)}%</Html>;
   }
 
   return (
@@ -49,7 +71,7 @@ const ModelFloor = ({ sole, stripe, back, top }) => {
           penumbra={0.01}
           castShadow
           shadow-bias={-0.01}
-          // shadow-mapSize={{ width: 512, height: 512 }}
+        // shadow-mapSize={{ width: 512, height: 512 }}
         />
 
         {/* Ambient light */}
